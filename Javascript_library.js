9abec8a640741d05a977878a125fbc4c13525cd2
Soobin Kim <kim.soobin24@gmail.com>
2017-05-10 18:16:26 -0700

    finished assignment

--- Javascript_library.js
+++ Javascript_library.js
@@ -1,14 +1,3 @@
-//     each: function(arr, callback) {
-//     // loop through the array
-//       for(var i = 0; i < arr.length; i++) {
-//         callback(arr[i]); // invoking the callback many times... delegation!
-//       }
-//     }
-//
-//     each([1,2,3], function(num) { alert(num + " I am from the callback!"); }) //so many alerts!
-
-
-
 var _ = {
    map: function(array, callback){
      var xarray = [];
@@ -22,32 +11,41 @@ var _ = {
      for(var i = 0; i < list.length; i++){
        sum = callback(sum, list[i])
      }
-
+     return sum;
    },
-      find: function(list,callback) {
-        for(var i = 0; i < list.length; i++){
-          if (callback(list[i])){
-            return list[i];
-          }
+    find: function(list,callback) {
+      for(var i = 0; i < list.length; i++){
+        if (callback(list[i])){
+          return list[i];
         }
-        return false;
-        // code here;
-      },
-}
-var iarray = ['soobin', 'beatrice', 'john']
-var hello = _.map(iarray, function(x){return x + " is on a windows machine"});
-console.log(hello)
+      }
+      return false;
+      // code here;
+    },
+     filter: function(arr, callback) {
+       var arr1 = [];
+       for(var i = 0; i < arr.length; i++){
+         arr1 = callback(arr1[i]);
+         return arr1;
+       }
+       return false;
+     },
+     reject: function(arr, callback) {
+       var arrx = [];
+       for(var i = 0; i < arr.length; i++){
+         arrx = callback(!arrx[i]);
+         return arrx;
+       }
+       return false;
+     },
+   }
+
+console.log(_.map(['soobin', 'conan', 'john'], function(i){return i + " is on a windows machine"}));
 
 console.log(_.find([1,2,4,5,-10,5], function(p){return p % 2 == 1}));
 
-console.log(_.reduce([1,2,4,5], function(a){return a, callback))
+console.log(_.reduce([1,2,4,5], function(a, callback){return a + callback;}));
 
+console.log(_.filter([1,5,8,-15,10], function(x){return x === 4}))
 
-//    filter: function() {
-//      // code here;
-//    },
-//    reject: function() {
-//      // code here;
-//    }
-//  }
-// // you just created a library with 5 methods!
+console.log(_.reject([1,2,10,-10,5,8,20], function(x){return x > 10}))
